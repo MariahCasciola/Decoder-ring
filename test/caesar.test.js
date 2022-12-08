656cf9df0fbcfc6d467f33388c5fb1a1e45a886a
MariahCasciola <117303840+MariahCasciola@users.noreply.github.com>
2022-12-08 14:19:55 -0600

    2 tests for caesar for shift and a skeleton

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,50 +1,45 @@
 // Write your tests here!
 const { expect } = require("chai");
 const caesarModule = require("../src/caesar"); //imported caesar function
-
-describe("tests for caesar shift", () => {
-  describe("errors to catch", () => {
-    it("should test if shift value isn't present, if not return false", () => {
+// All tests
+describe("mariah's tests for caesar shift", () => {
+  // Section for shift error tests
+  describe("errors for shift", () => {
+    it("if we get a shift value of zero, we should return false", () => {
       const message = "boo boo";
       const shift = 0;
-      const actual = false;
-      // const expected =
-      expect(actual).to.be.equal(false);
+      const actual = caesar(message, shift);
+      expect(actual).to.be.false;
     });
 
-    it("should return false if the shift is above 25", () => {
+    it("if we don't have shift value, we should return false", () => {
       const message = "boo boo";
-      const shift = 26;
-      const actual = false;
-      expect(actual).to.be.equal(false);
-    });
-
-    it("should return false if the shift is below -25", () => {
-      const message = "boo boo";
-      const shift = -26;
-      const actual = false;
-      expect(actual).to.be.equal(false);
+      const actual = caesar(message);
+      expect(actual).to.be.false;
     });
   });
 
-  describe("caesar function decoding a message", () => {
-    it("should encode a message by shifting the letters", () => {});
-    it("should include spaces and other symbols in messages", () => {});
-    it("should ignore capital letters", () => {});
-    it("should appropriately enocode or decode letters after the end of the alphabet", () => {});
-    it("should process a negative shift, going left", () => {});
+  // Section for encoding tests
+  describe("encoding", () => {
+    it("first test", () => {
+      //Write test here
+      expect(something).to.be.something;
+    });
+
+    it("second test", () => {
+      //Write test here
+      expect(something).to.be.something;
+    });
   });
-  describe("", () => {});
-  it("should decode a message, shifting letters in the opposite direction", () => {});
-  it("should leaves spaces and other symbols as is", () => {});
-  it("should ignore capital letters", () => {});
-  it("", () => {});
-  it("", () => {});
-  it("", () => {});
-  it("", () => {});
-  it("", () => {});
-  it("", () => {});
-  it("", () => {});
-  it("", () => {});
-  it("", () => {});
+
+  // Section for Decoding Tests
+  describe("decoding", () => {
+    it("first test", () => {
+      expect(something).to.be.something;
+    });
+
+    it("second test", () => {
+      expect(something).to.be.something;
+    });
+  });
 });
